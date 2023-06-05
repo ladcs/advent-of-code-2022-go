a4b16f506c3e0800f3dbc4211da441b0754b8de8
ladcs <hpladcs@gmail.com>
2023-06-05 15:19:03 -0300

    feat part1 day1

---
+++ day1/part1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	higherCalories := 0
+	calories := 0
+	data, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer data.Close() // fecha o arquivo após o fim da função
+
+	scanner := bufio.NewScanner(data)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			if higherCalories < calories {
+				higherCalories = calories
+			}
+		} else {
+			kcl, errAtoi := strconv.Atoi(line)
+
+			if errAtoi != nil {
+				log.Fatal(errAtoi)
+			}
+
+			calories += kcl
+		}
+	}
+
+	fmt.Println(higherCalories)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module advenctOfCode
+
+go 1.20
